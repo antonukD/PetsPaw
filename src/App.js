9595c29ac4a5f52f35047c5db38534506d79b413
Antonuk Daniel <antonuk.d@gmail.com>
2023-08-13 19:22:43 +0300

    removing routing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './components/pages/Home'
-import Breeds from './components/pages/Breeds'
-import Gallery from './components/pages/Gallery'
-import Voiting from './components/pages/Voiting'
-import NotFound from './components/pages/NotFound'
-import MaineLayouts from './components/Layouts/MaineLayouts';
+import React, { useState, useEffect } from 'react'
+// import { Outlet } from 'react-router-dom'
+import Menu from './components/Navigation/Menu'
 import './App.css';
 
+
 function App() {
+
+  // const [dimensions, setDimensions] = useState({
+  //   width: window.innerWidth,
+  //   height: window.innerHeight,
+  // })
+  // console.log(dimensions)
+  // const handleResize = () => {
+  //   setDimensions({
+  //     width: window.innerWidth,
+  //     height: window.innerHeight,
+  //   })
+  // }
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize, false)
+  // }, [])
+  // useEffect(() => {}, [dimensions])
+
+
   return (
-    <BrowserRouter>
+
       <div className="App dark">
         <body className='bg-page-color dark:bg-[#1D1D1D] '>
-        <Routes>
-          <Route path="/" element={<MaineLayouts />}>
-            <Route index element={<Home />} />
-            <Route path="breeds" element={<Breeds />} />
-            <Route path="gallery" element={<Gallery />} />
-            <Route path="voiting" element={<Voiting />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes> 
+          <div className="flex justify-center lg:grid lg:grid-cols-12 h-screen w-screen overflow-hidden z-20">
+            <div className="flex flex-col justify-between lg:col-span-4 lg:col-start-2 w-auto h-auto">
+              <Menu />
+            </div>
+            {/* <Outlet /> */}
+          </div>      
         </body>
       </div>
-    </BrowserRouter>
+
   );
 }
 
